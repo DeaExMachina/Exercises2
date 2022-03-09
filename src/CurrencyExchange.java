9f881c14dbce2bf3ef48a66ac26ef41f756fe4f7
DeaExMachina <sister_of_night@gmx.at>
2022-03-09 11:11:45 +0100

    .

--- src/CurrencyExchange.java
+++ src/CurrencyExchange.java
@@ -1,9 +1,40 @@
-//public class CurrencyExchange {
-//    //public static void main(String[] args) {
-//
-//    }
-//
-//    //public static double exchangeCurrency(String currencyID, double value) {
-//
-//    }
-//}
+public class CurrencyExchange {
+    public static void main(String[] args) {
+
+        exchangeCurrency("SEK", 200);
+
+
+    }
+
+    public static double exchangeCurrency(String currencyID, double value) {
+        double conversionRate;
+        double newValue = 0;
+
+        switch (currencyID) {
+            case "HUF" -> {
+                conversionRate = 328.61;
+                newValue = value * conversionRate;
+                System.out.println("For your " + value + "€, you get " + newValue + "€ in HUF.");
+            }
+            case "SEK" -> {
+                conversionRate = 10.76;
+                newValue = value * conversionRate;
+                System.out.println("For your " + value + "€, you get " + newValue + "€ in SEK.");
+            }
+            case "USD" -> {
+                conversionRate = 1.12;
+                newValue = value * conversionRate;
+                System.out.println("For your " + value + "€, you get " + newValue + "€ in USD.");
+            }
+            case "CAD" -> {
+                conversionRate = 1.47;
+                newValue = value * conversionRate;
+                System.out.println("For your " + value + "€, you get " + newValue + "€ in CAD.");
+            }
+            default -> System.out.println("Please provide a correct String!");
+        }
+
+        return newValue;
+    }
+}
+
---
+++ src/PerfectNumbers.java
@@ -0,0 +1,17 @@
+//public class PerfectNumbers {
+//    public static void main(String[] args) {
+//        int i = 0;
+//
+//        while (i <= 100000000) {
+//            if (i %1 == 0 && i %2 == 0 && i) {
+//
+//            }
+//            else {
+//
+//            }
+//            i++;
+//        }
+//
+//    }
+//}
+
